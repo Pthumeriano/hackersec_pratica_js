12b101722542ac820092c3007b4cc80748fe7395
Pthumeriano <helderandrade377@gmail.com>
2022-10-15 00:00:57 -0300

    Update algoritmos.js

--- pratica_js_hackersec/algoritmos.js
+++ pratica_js_hackersec/algoritmos.js
@@ -1,68 +1,64 @@
-function escrever(coisa){
+function escrever(coisa) {
     document.getElementById("input").value += coisa;
 }
-function ce(){
+
+function ce() {
     document.getElementById("input").value = "";
 }
 
-function validar(operacao){
-    if(operacao[operacao.lenght-1] == '+' || operacao[operacao.lenght-1] == '-' || operacao[operacao.lenght-1] == '*' || operacao[operacao.lenght-1] == '/'){
-        return false;
-    }
-    if(operacao[0] == '+' || operacao[0] == '-' || operacao[0] == '*' || operacao[0] == '/'){
-        return false;
-    }
-    var qtdOperadores = 0;
-    for(let i=0; i<operacao.lenght; i++){
-        
-        if(operacao[i]=='+' || operacao[i]=='-' || operacao[i]=='*' || operacao[i]=='/'){
-            
-            qtdOperadores++;
-        }
-    }
+function calcular() {
 
-    console.log(qtdOperadores)
-    if(qtdOperadores>1){
-        return false;
-    }
-}
-
-function calcular(){
-
-    var operacao = document.getElementById("input").value;
+    let operacao = document.getElementById("input").value;
     var operador = "";
 
-    if(operacao.includes('+')){
-        operador='+';
+    if (operacao.includes('+')) {
+        operador = '+';
     }
-    if(operacao.includes('-')){
-        operador='-';
+    if (operacao.includes('-')) {
+        operador = '-';
     }
-    if(operacao.includes('*')){
-        operador='*';
+    if (operacao.includes('*')) {
+        operador = '*';
     }
-    if(operacao.includes('/')){
-        operador='/';
+    if (operacao.includes('/')) {
+        operador = '/';
     }
 
-    if(!validar(operacao)){
+    var qtdOperadores = 0;
+
+    for (let i = 0; i < 99; i++) {
+
+        try {
+            if ((operacao[i] == '+') || (operacao[i] == '-') || (operacao[i] == '*') || (operacao[i] == '/')) {
+                qtdOperadores++;
+            }
+        } catch {
+            alert('ERR')
+        }
+
+
+    }
+    console.log('operacao = ' + operacao)
+    console.log('tamanho da operacao = ' + operacao.lenght)
+    console.log('qtdOperadores = ' + qtdOperadores)
+
+    if (qtdOperadores == 1) {
         let op = operacao.split(operador);
-        if(operador=='+'){
+        if (operador == '+') {
             document.getElementById("input").value = parseFloat(parseFloat(op[0]) + parseFloat(op[1]));
         }
-        if(operador=='-'){
+        if (operador == '-') {
             document.getElementById("input").value = parseFloat(parseFloat(op[0]) - parseFloat(op[1]));
         }
-        if(operador=='*'){
+        if (operador == '*') {
             document.getElementById("input").value = parseFloat(parseFloat(op[0]) * parseFloat(op[1]));
         }
-        if(operador=='/'){
+        if (operador == '/') {
             document.getElementById("input").value = parseFloat(parseFloat(op[0]) / parseFloat(op[1]));
         }
 
-    }else{
+    } else {
         alert("ERR")
     }
 
 }
-
